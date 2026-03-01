b7d89f35f9eff5c9db42f729f8cc8dba0f924b23
dgTr3 <Edugta31@gmail.com>
2026-03-01 15:31:35 -0600

    completo

--- While_Ejercicio20/Program.cs
+++ While_Ejercicio20/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Contar cuántos dígitos tiene un número.");
+Console.WriteLine("Ingrese número");
+int numero = int.Parse(Console.ReadLine());
+int contador = 0;
+while (numero > 0)
+{
+    numero /= 10;
+    contador++;
+}
+Console.WriteLine("cantidad de digitos " + contador);
